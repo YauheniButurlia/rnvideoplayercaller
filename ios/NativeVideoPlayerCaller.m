352b002e4dbbac328d97854527cb87f0d2f61d8d
Yauheni Buturlia <e.buturlia@softteco.com>
2019-10-26 08:48:35 +0300

    ios ui thread fix

--- ios/NativeVideoPlayerCaller.m
+++ ios/NativeVideoPlayerCaller.m
@@ -8,22 +8,22 @@ RCT_EXPORT_MODULE()
 RCT_EXPORT_METHOD(showVideoPlayer: (NSString*) url)
 {
     self.videoURL = [NSURL URLWithString:url];
-    UIViewController *rootViewController = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
-
-    AVPlayer *player = [AVPlayer playerWithURL:self.videoURL];
-    self.playerViewController = [AVPlayerViewController new];
-    _playerViewController.player = player;
-    _playerViewController.showsPlaybackControls = YES;
-
 
     dispatch_async(dispatch_get_main_queue(), ^{
+
+        UIViewController *rootViewController = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+
+        AVPlayer *player = [AVPlayer playerWithURL:self.videoURL];
+        self.playerViewController = [AVPlayerViewController new];
+        _playerViewController.player = player;
+        _playerViewController.showsPlaybackControls = YES;
 
         [rootViewController.view addSubview:self.playerViewController.view];
         [rootViewController presentViewController:self.playerViewController animated:YES completion:nil];
 
+        _playerViewController.player.play;
+        
     });
-
-    _playerViewController.player.play;
 }
 
 @end
